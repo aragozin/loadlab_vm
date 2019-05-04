cb741d831a49d415da64b00154aa75d1a864f85a
Alexey Ragozin <alexey.ragozin@gmail.com>
2019-05-04 21:41:01 +0300

    ...

--- ansible/provision.ubuntu.sh
+++ ansible/provision.ubuntu.sh
@@ -1,5 +1,6 @@
 ﻿sudo apt-get -y install ansible
 
+ansible-playbook /home/boss/setup/ansible/provision.ubuntu/init-lxd.yml
+
 ansible-playbook /home/boss/setup/ansible/provision.ubuntu/init-bridge-adapter.yml
 
-ansible-playbook /home/boss/setup/ansible/provision.ubuntu/init-lxd.yml
