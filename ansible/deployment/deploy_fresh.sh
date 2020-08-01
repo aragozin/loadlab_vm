20cc86cdd234319b34f17802d89737d269814eae
Alexey Ragozin <alexey.ragozin@gmail.com>
2020-08-01 22:25:09 +0300

    ..

---
+++ ansible/deployment/deploy_fresh.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+set -e
+
+base_path=$( cd "$(dirname "${BASH_SOURCE[0]}")/.." ; pwd -P )
+
+cd $base_path
+
+if [ $# -eq 0 ]
+  then
+    ansible-playbook -i loadlab.hosts --extra-vars "wp_db_import_dump=true" loadlab.yml
+  else
+    ansible-playbook -i loadlab.hosts --extra-vars "wp_db_import_dump=true" loadlab.yml -l $1
+fi
+
