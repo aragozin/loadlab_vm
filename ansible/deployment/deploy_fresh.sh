f6bfe5c1b260da046bdfbcf67a261e24ee24efda
Alexey Ragozin <alexey.ragozin@gmail.com>
2020-08-01 23:42:33 +0300

    ..

--- ansible/deployment/deploy_fresh.sh
+++ ansible/deployment/deploy_fresh.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-base_path=$( cd "$(dirname "${BASH_SOURCE[0]}")/.." ; pwd -P )
+base_path=$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
+
+echo $base_path
 
 cd $base_path
 
@@ -10,6 +12,5 @@ if [ $# -eq 0 ]
   then
     ansible-playbook -i ${base_path}/loadlab.hosts --extra-vars "wp_db_import_dump=true" ${base_path}/loadlab.yml
   else
-    ansible-playbook -i ${base_path}/loadlab.hosts --extra-vars "wp_db_import_dump=true" ${base_path}/loadlab.yml -l $1
+    ansible-playbook -i ${base_path}/loadlab.hosts --extra-vars "wp_db_import_dump=true"${base_path}/loadlab.yml -l $1
 fi
-
