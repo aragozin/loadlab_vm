f7252bb57353ff302e2c9e6dba19290ab2af525e
Alexey Ragozin <alexey.ragozin@gmail.com>
2020-10-04 17:41:37 +0300

    ..

--- ansible/deployment/roles/wordpress/templates/wp-config.php
+++ ansible/deployment/roles/wordpress/templates/wp-config.php
@@ -35,6 +35,8 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('WP_ALLOW_REPAIR', true);
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
