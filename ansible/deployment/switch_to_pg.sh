281ee5f4511f4135920e4991eeda606147c55f6f
Alexey Ragozin <alexey.ragozin@gmail.com>
2020-10-04 20:24:55 +0300

    ..

--- ansible/deployment/switch_to_pg.sh
+++ ansible/deployment/switch_to_pg.sh
@@ -8,4 +8,4 @@ echo $base_path
 
 cd $base_path
 
-ansible-playbook -i ${base_path}/loadlab.hosts --extra-vars "wp_db_type=mysql" ${base_path}/loadlab.switch-db.yml
+ansible-playbook -i ${base_path}/loadlab.hosts --extra-vars "wp_db_type=postgres" ${base_path}/loadlab.switch-db.yml
