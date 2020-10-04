916c9ece98a327a8864aebbf6f90d70f26b53380
Alexey Ragozin <alexey.ragozin@gmail.com>
2020-10-05 00:25:10 +0300

    debug switch

---
+++ ansible/deployment/switch_debug_off.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+base_path=$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
+
+echo $base_path
+
+cd $base_path
+
+ansible-playbook -i ${base_path}/loadlab.hosts --extra-vars "wp_debug_switch=on" ${base_path}/loadlab.switch-wp-debug.yml
