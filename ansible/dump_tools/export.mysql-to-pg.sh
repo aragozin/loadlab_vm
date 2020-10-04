83e19dea049610434ada45ae5b280e59f731bfa1
Alexey Ragozin <alexey.ragozin@gmail.com>
2020-10-04 17:13:25 +0300

    SQL dump tools

--- ansible/dump_tools/export.mysql-to-pg.sh
+++ ansible/dump_tools/export.mysql-to-pg.sh
@@ -1,1 +1,24 @@
-﻿
+﻿#!/bin/sh
+
+ssh database << EOF
+
+yum install pgloader
+dropdb wordpress2 || true
+
+createdb wordpress2
+
+pgloader mysql://root@localhost/wordpress postgresql:///wordpress2
+
+psql wordpress2 << EOS
+
+ALTER DATABASE "wordpress" OWNER TO "wordpress";
+
+EOS
+
+EOF
+
+echo Export complete to wordpress2 db
+
+ssh database pg_dump postgresql:///wordpress2 > wp.dump
+
+ssh database dropdb wordpress2
