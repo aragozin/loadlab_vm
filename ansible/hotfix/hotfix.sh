503d64875199a0073d13cd1fe1d24a0aa9a62781
Alexey Ragozin <alexey.ragozin@gmail.com>
2020-10-16 06:45:31 +0300

    Hot fix

--- ansible/hotfix/hotfix.sh
+++ ansible/hotfix/hotfix.sh
@@ -5,5 +5,6 @@ base_path=$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 echo Update metrics
 
 $base_path/../deployment/deploy.sh metrics
+$base_path/../deployment/deploy.sh front
 
 echo Hotfix complete
